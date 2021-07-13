aa3509518c31fe4ccdd6df53df4aa65eed4316c3
libin <449196538@qq.com>
2021-07-13 18:34:57 +0800

    commit

---
+++ src/main/java/com/padingpading/multithreading/mult/MyThreadCallable.java
@@ -0,0 +1,38 @@
+package com.padingpading.multithreading.mult;
+
+import java.util.concurrent.*;
+
+/**
+ * @author libin
+ * @description callable方式实现
+ * @date 2021-07-12
+ */
+public class MyThreadCallable {
+
+    public static void main(String[] args) {
+        MyThread call = new MyThread();
+        FutureTask<String> task = new FutureTask<>(call);
+        Thread t = new Thread(task);
+        t.start();
+        try {
+            //获取当前线程的值
+            System.out.println(task.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+//实现Callable接口
+class MyThread implements Callable<String> {
+    @Override
+    public String call() {
+        String s = "使用Callable来返回值";
+        return s;
+    }
+}
+
+
--- src/test/java/com/padingpading/multithreading/MultithreadingApplicationTests.java
+++ src/test/java/com/padingpading/multithreading/MultithreadingApplicationTests.java
@@ -10,4 +10,5 @@ class MultithreadingApplicationTests {
     void contextLoads() {
     }
 
+
 }
