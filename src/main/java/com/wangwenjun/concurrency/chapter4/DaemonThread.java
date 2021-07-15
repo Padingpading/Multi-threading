5d94a9ec765425fdb16fd7c805dc9ef2f0e4a5fe
libin <449196538@qq.com>
2021-07-15 18:49:59 +0800

    commit

---
+++ src/main/java/com/wangwenjun/concurrency/chapter4/DaemonThread.java
@@ -0,0 +1,28 @@
+package com.wangwenjun.concurrency.chapter4;
+
+import java.net.HttpRetryException;
+
+/***************************************
+ * @author:Alex Wang
+ * @Date:2017/2/17 QQ:532500648
+ * QQ交流群:286081824
+ ***************************************/
+public class DaemonThread {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread main = Thread.currentThread();
+        Thread thread = new Thread(()->{
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.interrupt();
+    }
+}
+
+/**
+ * A<---------------------------------->B
+ *  ->daemonThread(health check)
+ * */
