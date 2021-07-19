6f9ffaddb64c08b881949fc791bec8b66acc71d1
libin <449196538@qq.com>
2021-07-19 22:43:13 +0800

    commit

--- src/main/java/com/padingpading/multithreading/MultithreadingApplication.java
+++ src/main/java/com/padingpading/multithreading/MultithreadingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultithreadingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MultithreadingApplication.class, args);
     }
---
+++ src/main/java/com/padingpading/multithreading/thread/execption/ThreadException.java
@@ -0,0 +1,29 @@
+package com.padingpading.multithreading.thread.execption;
+
+/**
+ * java 线程异常的捕捉
+ */
+public class ThreadException {
+
+    private final static int A = 10;
+
+    private final static int B = 0;
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            try {
+                Thread.sleep(5_000L);
+                int result = A / B;
+                System.out.println(result);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t.start();
+        t.setUncaughtExceptionHandler((thread, e) -> {
+            System.out.println(e);
+            System.out.println(thread);
+        });
+        System.out.println("dfdsfd");
+    }
+}
